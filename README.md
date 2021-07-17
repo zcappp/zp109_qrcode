cf446445f85ad0d1d3ff1cd3b709c2f01bf05813
众触应用平台 <87555332+zclc@users.noreply.github.com>
2021-07-17 20:53:22 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# 二维码生成器
+
+众触零码应用平台插件
+
+Plugins For ZhongChu ZeroCode Application Platform
---
+++ plugins/zp109_二维码生成器.js
@@ -0,0 +1,26 @@
+function onInit({ exc, props, container }) {
+    exc('load("//z.zcwebs.cn/vendor/qrcode.min.js")', {}, () => {
+        new QRCode(container, {
+            text: props.text || location.href,
+            width: props.width || 128,
+            height: props.width || 128,
+            correctLevel: QRCode.CorrectLevel.H
+        })
+    })
+}
+
+$plugin({
+    id: "zp109",
+    props: [{
+        prop: "text",
+        type: "text",
+        label: "文字内容",
+        ph: "默认是当前URL(location.href)"
+    }, {
+        prop: "width",
+        type: "number",
+        label: "宽度(px)",
+        ph: "默认是128"
+    }],
+    onInit
+})
